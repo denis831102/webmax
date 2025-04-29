ea7ec198249e96c4c32d154e4e52f610279ac5ed
denis831102 <denis831102@gmail.com>
2025-04-29 11:37:38 +0300

    change operation 2

--- src/components/EP/Bits/eDialog_Operation.vue
+++ src/components/EP/Bits/eDialog_Operation.vue
@@ -11,29 +11,35 @@
       label-width="auto"
       style="max-width: 600px; border: 0"
     >
-      <el-form-item label="Пункт">
-        <el-tag type="info">{{ form.namePunkt }}</el-tag>
-      </el-form-item>
-
-      <el-form-item label="Прізвище">
-        <el-tag type="primary">{{ form.nameUser }}</el-tag>
+      <el-form-item label="Пункт / прізвище">
+        <el-col :span="15">
+          <el-tag type="primary" style="width: 100%">{{
+            form.namePunkt
+          }}</el-tag>
+        </el-col>
+        <el-col :span="1" style="text-align: center">
+          <span>/</span>
+        </el-col>
+        <el-col :span="8">
+          <el-tag type="primary">{{ form.nameUser }}</el-tag>
+        </el-col>
       </el-form-item>
 
       <el-form-item label="Дата операції">
         <el-col :span="11">
           <el-date-picker
-            v-model="form.date1"
+            v-model="form.date"
             type="date"
             placeholder="Pick a date"
             style="width: 100%"
           />
         </el-col>
-        <el-col :span="2" class="text-center">
-          <span class="text-gray-500">-</span>
+        <el-col :span="2" style="text-align: center">
+          <span>-</span>
         </el-col>
         <el-col :span="11">
           <el-time-picker
-            v-model="form.date2"
+            v-model="form.time"
             placeholder="Pick a time"
             style="width: 100%"
           />
@@ -43,25 +49,50 @@
       <el-form-item label="Операція">
         <el-col :span="24">
           <el-cascader
-            v-model="operation"
-            :options="options"
+            v-model="selOperation"
+            :options="form.options"
             :props="multCascader"
             @change="handleChange"
             clearable
             collapse-tags
             collapse-tags-tooltip
-            :max-collapse-tags="3"
+            :max-collapse-tags="2"
             placeholder="зробіть вибір..."
             style="width: 100%"
           />
         </el-col>
       </el-form-item>
 
-      <el-form-item>
-        <el-table :data="tableData" height="250" style="width: 100%">
-          <el-table-column prop="date" label="Дія" />
-          <el-table-column prop="name" label="Значення" />
-          <el-table-column prop="address" label="Коментар" />
+      <el-form-item style="box-shadow: 0px -1px 6px 2px #b0b3b7">
+        <el-table
+          :data="tableOperation"
+          border="true"
+          height="250"
+          style="width: 100%; margin: 5px; font-size: 9pt"
+        >
+          <el-table-column prop="nameOperation" label="Операція" width="190" />
+          <el-table-column label="Кількість">
+            <template #default="props">
+              <el-input-number
+                v-model="props.row.count"
+                :precision="2"
+                :step="0.1"
+                :min="0"
+                size="small"
+                style="width: 95%; height: 40px"
+              />
+            </template>
+          </el-table-column>
+          <el-table-column label="Ціна одиниці">
+            <template #default="props">
+              <el-input v-model="props.row.price" clearable />
+            </template>
+          </el-table-column>
+          <el-table-column label="Сума" width="70">
+            <template #default="props">
+              {{ props.row.summa }}
+            </template>
+          </el-table-column>
         </el-table>
       </el-form-item>
 
@@ -84,6 +115,7 @@ import {
   defineProps,
   defineEmits,
   ref,
+  watchEffect,
   computed,
   onUpdated,
 } from "vue";
@@ -99,68 +131,70 @@ const emit = defineEmits([]);
 const store = useStore();
 const getCurUser = computed(() => store.getters.getCurUser);
 // const setting = inject("setting");
-const operation = ref([]);
+const selOperation = ref([]);
 const form = reactive({
   nameUser: getCurUser.value.PIB,
   namePunkt: "",
-  date1: "",
-  date2: "",
-  delivery: false,
-  type: [],
-  resource: "",
+  date: "",
+  time: "",
   coment: "",
+  options: [],
 });
-const options = ref([]);
 const multCascader = { multiple: true };
-
-const tableData = [
-  {
-    date: "2016-05-03",
-    name: "Tom",
-    address: "No. 189, ",
-  },
-  {
-    date: "2016-05-02",
-    name: "Tom",
-    address: "No. 189, ",
-  },
-  {
-    date: "2016-05-04",
-    name: "Tom",
-    address: "No. 189, ",
-  },
-  {
-    date: "2016-05-01",
-    name: "Tom",
-    address: "No. 189, ",
-  },
-  {
-    date: "2016-05-08",
-    name: "Tom",
-    address: "No. 189, ",
-  },
-  {
-    date: "2016-05-06",
-    name: "Tom",
-    address: "No. 189, ",
-  },
-  {
-    date: "2016-05-07",
-    name: "Tom",
-    address: "No. 189, ",
-  },
-];
+const tableOperation = ref([]);
 
 const saveData = () => {
   emit("update:visible", false);
   ElMessage.success("Дані будуть збережені");
 };
 
+// const tableOperation = computed(() =>
+//   selOperation.value.map((curOper) => {
+//     return {
+//       nameOperation: [
+//         form.options[curOper[0].num].label,
+//         form.options[curOper[0].num].children[[curOper[1].num]].label,
+//         form.options[curOper[0].num].children[[curOper[1].num]].children[
+//           [curOper[2].num]
+//         ].label,
+//       ].join(" / "),
+//       count: 100,
+//       price: 10,
+//       summa: 0,
+//     };
+//   })
+// );
+
 const handleChange = () => {
-  //console.log(value);
-  console.log(operation.value);
-  // ElMessage.success(operation.value);
+  // console.log(selOperation.value);
+  tableOperation.value = selOperation.value.map((curOper) => {
+    return {
+      nameOperation: [
+        form.options[curOper[0].num].label,
+        form.options[curOper[0].num].children[[curOper[1].num]].label,
+        form.options[curOper[0].num].children[[curOper[1].num]].children[
+          [curOper[2].num]
+        ].label,
+      ].join(" / "),
+      count: 100,
+      price: curOper[2].id,
+      summa: 0,
+    };
+  });
 };
+
+watchEffect(() => {
+  tableOperation.value.forEach((el) => {
+    el.summa = (el.count * el.price).toFixed(2);
+  });
+});
+
+// watch(
+//   () => tableOperation.value[0].count,
+//   () => {
+//     tableOperation.value[0].summa = 45;
+//   }
+// );
 
 const handleClose = () => {
   emit("update:visible", false);
@@ -174,7 +208,7 @@ const getOperation = async () => {
       },
     });
 
-    options.value = response.data;
+    form.options = response.data;
     ElMessage.success("Операції оновлені");
   } catch (e) {
     ElMessage("Помилка завантаження операцій");
