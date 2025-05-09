28f7197ec0980344a799002fd9f69de1fbbccbfd
denis831102 <denis831102@gmail.com>
2025-05-09 09:39:41 +0300

    add ecp

--- src/hooks/http.js
+++ src/hooks/http.js
@@ -1,11 +1,14 @@
 import axioc from "axios";
 
 //let base64 = btoa("username:password");
+//const curDate = new Date();
+const ecp = "c3875d07f44c422f3b3bc019c23e16ae";
 
 export const HTTP = axioc.create({
   baseURL: "https://webmax.lond.lg.ua/php/Server.php",
   headers: {
-    Ecp: "c3875d07f44c422f3b3bc019c23e16ae",
+    Ecp: ecp,
+    //Ecp: btoa(`${ecp}:${curDate}`),
     Token: "",
   },
 });
