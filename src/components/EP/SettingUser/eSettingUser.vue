847544e3254ecc81c2a7451159fddbf76b75caac
denis831102 <denis831102@gmail.com>
2025-05-13 06:45:54 +0300

    change tittle settingUser

--- src/components/EP/SettingUser/eSettingUser.vue
+++ src/components/EP/SettingUser/eSettingUser.vue
@@ -1,19 +1,28 @@
 <template>
-  <el-card style="max-width: 380px">
-    <el-text class="mx-1"
-      >Оберіть стовбчик, в який Ви будете вносити дані</el-text
-    >
-    <el-radio-group
-      v-model="colOper"
-      @change="changeCol"
-      size="large"
-      fill="#6cf"
-      style="margin-top: 10px"
-    >
-      <el-radio-button label="загальна сума" value="summa" />
-      <el-radio-button label="вартість одиниці (кг/т)" value="price" />
-    </el-radio-group>
-  </el-card>
+  <el-space
+    fill
+    wrap
+    fill-ratio="50"
+    direction="horizontal"
+    style="width: 100%"
+  >
+    <el-card style="max-width: 380px">
+      <el-text class="mx-1"
+        >Оберіть стовбчик, в який Ви будете вносити дані в операції на власному
+        пункті</el-text
+      >
+      <el-radio-group
+        v-model="colOper"
+        @change="changeCol"
+        size="large"
+        fill="#6cf"
+        style="margin-top: 10px"
+      >
+        <el-radio-button label="ціна одиниці (кг/т)" value="price" />
+        <el-radio-button label="загальна сума" value="summa" />
+      </el-radio-group>
+    </el-card>
+  </el-space>
 </template>
 <script setup>
 import { ref, onMounted, computed } from "vue";
