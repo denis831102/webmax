bdcd3b631074c8b2b4e4d0148e3dab100854273e
Den <denis831102@gmail.com>
2025-09-05 17:13:08 +0300

    change router block

--- src/router/index.js
+++ src/router/index.js
@@ -11,17 +11,26 @@ const router = createRouter({
   routes,
 });
 
-router.beforeEach((to) => {
+router.beforeEach((to, from, next) => {
   const store = useStore();
   const getAuthenticated = computed(() => store.getters.getAuthenticated);
+  const getAnswerServer = computed(() => store.getters.getAnswerServer);
   //const fetchUsers = () => store.dispatch("fetchUsers");
   //const changeAuthenticated = (val) => store.commit("changeAuthenticated", val);
   //const action = (val) => store.dispatch("action", val);
 
+  // if (to.meta.requiresAuth && !getAuthenticated.value) {
+  //   return {
+  //     name: "authent",
+  //   };
+  // }
+
+  // Если маршрут требует логина, а юзер не залогинен
   if (to.meta.requiresAuth && !getAuthenticated.value) {
-    return {
-      name: "authent",
-    };
+    next(getAnswerServer.value.status ? "/authent" : "/block");
+  } else {
+    // Иначе разрешаем переход
+    next();
   }
 });
 
